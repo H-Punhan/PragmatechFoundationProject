f347e0df5b90b8f4810ac1763443b8b7cc1c8c3b
Punhan Huseynov <husepunhan@gmail.com>
2020-12-24 22:56:13 +0400

    12 column grid system added

--- todolist/index.js
+++
@@ -1,7 +0,0 @@
-let addbutton=document.getElementById('add-button')
-let text=document.getElementById('text')
-let deletebutton=document.getElementById('delete-button')
-
-addbutton.addEventListener('click',()=>{
-   document.getElementById('todo-div').innerHTML+="<div id='text-div'><input type='checkbox' class='check'><p>"+text.value+"</p></div>";
-})
