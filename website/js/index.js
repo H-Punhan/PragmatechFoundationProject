86df75a8472caca5bf55f29540cc91e9fd1ed35e
Punhan Huseynov <husepunhan@gmail.com>
2021-01-24 10:10:30 +0400

    Dinamik navbar elave olundu

--- website/js/index.js
+++ website/js/index.js
@@ -6,9 +6,21 @@ let responsive_button=document.getElementById('responsive-menu-button');
 let responsive_menu=document.getElementById('responsive-menu');
 
 
+
+
 let img=document.getElementById('img')
 let imgx=0;
 let imgy=0;
+
+//  a links for page change
+let navbarIcon=document.querySelectorAll('#navbar-nav-icons a');
+let responsivemenuIcon=document.querySelectorAll('#responsive-menu-links a')
+let postIcon=document.querySelectorAll('#blog-div-posts a')
+let changeArrrows=document.querySelectorAll("#navbar-nav-arrows a")
+// all page classes
+let changePagediv=document.querySelectorAll('.changePage')
+let pageindex=0
+
 
 
 
@@ -20,6 +32,30 @@ responsive_button.addEventListener('click',()=>{
     openMenu();
 
 })
+
+for(let i=0;i<navbarIcon.length;i++){
+
+    navbarIcon[i].addEventListener('click',()=>{
+        changePage(navbarIcon[i].id)
+    })
+    responsivemenuIcon[i].addEventListener('click',()=>{
+        changePage(responsivemenuIcon[i].id)
+    })
+    postIcon[i].addEventListener('click',()=>{
+        changePage(postIcon[i].id)
+        
+        
+    })
+
+}
+
+for(let i=0;i<changeArrrows.length;i++){
+
+    changeArrrows[i].addEventListener('click',()=>{
+        changePage2(changeArrrows[i])
+    })
+
+}
 
 // functions
 // responsive menu function
@@ -57,74 +93,86 @@ function openMenu(){
 
 
 
-//  img effect function 
-// window.addEventListener('mousemove',(mouse)=>{
-    
-//     if(window.innerWidth>1200){
-
-    
-//     img.style.backgroundPositionX+=mouse.offsetX;
-    
-//     console.log(img.style.backgroundPositionX)
-    
+// change page 
+function changePage(navbarIcona){
     
     
 
+    for(let i=0;i<4;i++){
 
-//     // if(mouse.offsetX>window.innerWidth/2){
+        if(changePagediv[i].className==navbarIcona+" changePage"){
+
+            changePagediv[i].style.height='100%';
+            
+            pageindex=i
+
+            navbarIcon[i].style.color='#04B4E0';
+           
+            
+        }
+        else{
+            changePagediv[i].style.height='0%';
+            navbarIcon[i].style.color='black';
+            openMenu()
+            
+          
+            
+
+            
+
+        }
+
+    }
+}
+function changePage2(buttonvalue){
+    
+    
+    
+    if(buttonvalue.id=='navbar-arrow-right'){
+
         
-//     //     if(imgx<5){
-//     //         imgx+=1
-//     //         console.log(imgx)
-//     //     }
-
-        
-        
-//     //     img.style.backgroundPositionX=imgx+"px";
+        changePagediv[pageindex].style.height='0%';
         
 
-//     // }
+        pageindex++;
 
-//     // else{
+        if(pageindex>3){
+            pageindex=0;
+        }
+
+        changePagediv[pageindex].style.height='100%';
+        
         
 
-//     //     if(imgx>-10){
-//     //         imgx-=1
-//     //         console.log(imgx)
-//     //     }
-//     //     img.style.backgroundPositionX=imgx+"px";
+    }
+        
 
-//     // }
+    else{
 
-//     if(mouse.offsetY>window.innerHeight/2){
+        changePagediv[pageindex].style.height='0%';
+
+        pageindex--;
+
+        if(pageindex<0){
+            pageindex=3;
+        }
+
+        changePagediv[pageindex].style.height='100%';
+
+    }
+
+    for(let i=0;i<navbarIcon.length;i++){
+
+       if(navbarIcon[i] ==navbarIcon[pageindex]){
+            navbarIcon[i].style.color="#04B4E0";
+       }
+       else{
+            navbarIcon[i].style.color="black";
+       }
        
-//         if(imgy<10){
-//             imgy+=1
-//             console.log(imgy)
-//         }
 
-        
-        
-//         img.style.backgroundPositionY=imgy+"px";
-        
-
-//     }
-
-//     else{
-        
-//         if(imgy>-10){
-//             imgy-=1
-//             console.log(imgy)
-//         }
-//         img.style.backgroundPositionY=imgy+"px";
-
-//     }
-// }
-// else{
-
-//     img.style.backgroundPositionY=0+"px";
-
-// }
-// })
+    }
 
 
+
+}
