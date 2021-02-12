61a598e74836f294477c1a2c50efa07072ade279
Punhan Huseynov <husepunhan@gmail.com>
2021-02-12 21:49:13 +0400

    Hackerrank tapsiriqlari ucun folder elave olundu

--- website/js/main.js
+++ website/js/main.js
@@ -14,11 +14,21 @@ let menuButton=document.getElementById('responsive-menu-button') //Button  for m
 
 let responsiveMenu=document.getElementById('responsive-menu') //Mobile menu
 
+let arrowButton=document.getElementsByClassName('arrow')
+
+
 
 
 
 // Adding Events 
 
+
+for(let i=0;i<arrowButton.length;i++){
+    arrowButton[i].addEventListener('click',()=>{
+        changePage(arrowButton[i])
+    })
+    
+}
 
 
 searchBarbutton.addEventListener('click',()=>{
@@ -94,6 +104,48 @@ function openMenu(){
         
         isOpenMenu=false
     }
+
+}
+
+
+function changePage(a){
+    let pages=['index.html','resume.html','portfolio.html','blog.html','contact.html']
+    url=window.location.pathname;
+    url=url.substring(url.lastIndexOf('/')+1)
+
+    if(a.id=='arrow-right'){
+        for(let i=0;i<pages.length;i++){
+            if(pages[i]==url){
+                console.log(pages[i])
+                pages[i]=pages[i+1]
+                if(pages[i]==undefined){
+                    pages[i]='index.html';
+                }
+                
+                
+                window.location.href=pages[i]
+                
+            }
+            
+        }
+    }
+    else{
+        for(let i=0;i<pages.length;i++){
+            if(pages[i]==url){
+                console.log(pages[i])
+                pages[i]=pages[i-1]
+                if(pages[i]==undefined){
+                    pages[i]='contact.html';
+                }
+                
+                
+                window.location.href=pages[i]
+                
+            }
+            
+        }
+    }
+    
 
 }
 
