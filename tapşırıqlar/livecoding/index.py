a25741eea4581acc155c742101330b8d5cc37118
Punhan Huseynov <husepunhan@gmail.com>
2021-02-25 08:55:35 +0400

    Tapsiriqlar elave olundu

--- tapşırıqlar/livecoding/index.py
+++ tapşırıqlar/livecoding/index.py
@@ -9,46 +9,46 @@ forfunc(lambda x:x%5==0)
 forfunc(lambda x:x>9)
 forfunc(lambda x:int(list(str(x))[0])+int(list(str(x))[0])>10)
 
+#------------------------------------
+isciler=[
+    {
+        'ad':'Memmed',
+        'maas':'600AZN'
+    },
+    {
+        'ad':'Cemile',
+        'maas':'500AZN'
+    },
+    {
+        'ad':'Saleh',
+        'maas':'1200AZN'
+    },
+    {
+        'ad':'Gulnar',
+        'maas':'980AZN'
+    }
+]
 
-# isciler=[
-#     {
-#         'ad':'Memmed',
-#         'maas':'600AZN'
-#     },
-#     {
-#         'ad':'Cemile',
-#         'maas':'500AZN'
-#     },
-#     {
-#         'ad':'Saleh',
-#         'maas':'1200AZN'
-#     },
-#     {
-#         'ad':'Gulnar',
-#         'maas':'980AZN'
-#     }
-# ]
+maaslar=['600AZN','500AZN','1200AZN','980AZN']
+maas2=[]
 
-# maaslar=['600AZN','500AZN','1200AZN','980AZN']
-# maas2=[]
-
-# def findnumber():
-#     maas=''
-#     for m in maaslar:
+def findnumber():
+    maas=''
+    for m in maaslar:
   
-#         m=list(m)
+        m=list(m)
         
-#         for m1 in m:
+        for m1 in m:
         
-#              if m1.isnumeric()==True:
-#                  maas+=str(m1)
+             if m1.isnumeric()==True:
+                 maas+=str(m1)
         
 
-#         maas2.append(int(maas))
-#         maas=''
-#     print(maas2)
+        maas2.append(int(maas))
+        maas=''
+    print(maas2)
 
-# findnumber()
+findnumber()
 
 
 
