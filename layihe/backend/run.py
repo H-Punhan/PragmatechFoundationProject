1e6ce3651ba10ad3cf986d61aaa2137bafa56763
Punhan Huseynov <husepunhan@gmail.com>
2021-03-03 19:13:17 +0400

    Flask math game

--- layihe/backend/run.py
+++ layihe/backend/run.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect
 
-# app=Flask(__name__)
+app=Flask(__name__)
 
-# @app.route('/')
-# def write():
-#     return 'hello world'
-# app.run()
+
+@app.route('/')
+def write():
+    return render_template('index.html')
+    
+@app.route('/result',methods=['GET','POST'])     
+def result():
+    if request.method=='POST':
+        if request.form['num1']=='' or request.form['num2'] ==' ':
+            return redirect('/')
+        else:
+            return render_template('result.html',result=int(request.form['num1'])+int(request.form['num2']))
+    else:
+        return redirect('/')
+    
+
+if __name__=='__main__':
+    app.run(debug=True)
 
