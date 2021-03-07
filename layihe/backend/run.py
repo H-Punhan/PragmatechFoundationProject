3a32348566126fc8a4188d52ecf925785ed9c761
Punhan Huseynov <husepunhan@gmail.com>
2021-03-07 17:27:17 +0400

    flask ile databasa-a qosuldu

--- layihe/backend/run.py
+++ layihe/backend/run.py
@@ -1,22 +1,44 @@
 from flask import Flask,render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db=SQLAlchemy(app)
+users=[]
+id=0
 
+class Posts(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(50))
+    content=db.Column(db.String(50))
 
-@app.route('/')
-def write():
-    return render_template('index.html')
-    
-@app.route('/result',methods=['GET','POST'])     
-def result():
-    if request.method=='POST':
-        if request.form['num1']=='' or request.form['num2'] ==' ':
-            return redirect('/')
-        else:
-            return render_template('index.html',result=int(request.form['num1'])+int(request.form['num2']))
+@app.route('/',methods=["GET",'POST'])
+def add():
+    if request.method=='POST' and request.form['title'] !='' and request.form['content'] !='' :
+
+        users=Posts(title=request.form['title'],content=request.form['content'])
+        db.session.add(users)
+        db.session.commit()
+        d=Posts.query.all()
+        return render_template('index.html',users=d)
     else:
-        return redirect('/')
+        d=Posts.query.all()
+        return render_template('index.html',users=d)
+
+@app.route('/delete/<deyer>')
+def delete(deyer):
+    suser=Posts.query.filter_by(id=deyer).first()
+    db.session.delete(suser)
+    db.session.commit()
+    return redirect('/')
     
+
+        
+    
+    
+
+ 
+
 
 if __name__=='__main__':
     app.run(debug=True)
